6cf7a69c1553f40617cf8eeb4895aaa137a11432
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-09-30 21:25:11 +0300

    Stage 2 homework 5

---
+++ src/main/kotlin/stage2/hw5/Main.kt
@@ -0,0 +1,11 @@
+package stage2.hw5
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println(scanner.nextInt())
+    println(scanner.nextInt())
+    println(scanner.nextInt())
+    println(scanner.nextInt())
+}
