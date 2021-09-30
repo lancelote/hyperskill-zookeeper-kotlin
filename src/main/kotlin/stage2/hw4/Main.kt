7a17764acba9fc565edee161fd3a0eef7ec40ddf
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-09-30 20:14:23 +0300

    Stage 2 homework 4

---
+++ src/main/kotlin/stage2/hw4/Main.kt
@@ -0,0 +1,18 @@
+package stage2.hw4
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val word1 = scanner.next()
+    val word2 = scanner.next()
+    val word3 = scanner.next()
+    val word4 = scanner.next()
+    val word5 = scanner.next()
+
+    println(word1)
+    println(word2)
+    println(word3)
+    println(word4)
+    println(word5)
+}
