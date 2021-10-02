28b52290e7e9bc8c3fbccaad92cba3fbe85bec03
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-10-02 16:57:47 +0300

    Stage 3 homework 2

---
+++ src/main/kotlin/stage3/hw2/Main.kt
@@ -0,0 +1,11 @@
+package stage3.hw2
+
+fun main() {
+    val word1 = readLine()
+    val word2 = readLine()
+    val word3 = readLine()
+    val word4 = readLine()
+    val word5 = readLine()
+
+    println("$word1 $word2 $word3 $word4 $word5")
+}
