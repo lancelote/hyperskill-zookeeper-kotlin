ffdd7ed15b36187883b8b67e5c374db28c329ab7
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-10-10 14:15:39 +0300

    Stage 3 homework 28

---
+++ src/main/kotlin/stage3/hw28/Main.kt
@@ -0,0 +1,11 @@
+package stage3.hw28
+
+fun main() {
+    val numbers = MutableList(100) { 0 }
+
+    numbers[0] = 1
+    numbers[9] = 10
+    numbers[99] = 100
+
+    println(numbers.joinToString())
+}
